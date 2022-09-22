366887c8a2f64e872c0da59f996586c7fd187fdb
Ko Nagase <nagase@georepublic.co.jp>
2022-09-22 10:11:19 +0900

    Generate shopping cart localizations

---
+++ features/shopping_cart/lib/src/l10n/generated/shopping_cart_localizations_ja.dart
@@ -0,0 +1,9 @@
+import 'shopping_cart_localizations.dart';
+
+/// The translations for Japanese (`ja`).
+class ShoppingCartLocalizationsJa extends ShoppingCartLocalizations {
+  ShoppingCartLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get shoppingCartPageTitle => 'ショッピングカート';
+}
